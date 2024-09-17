2f78dfc694db0db7238ec0cd277e0dd9ad90abb7
obiubah <sewpabee@gmail.com>
2024-09-17 23:22:45 +0100

    - refactor test - fix broken test for GET

--- spec/lib/memcache/process_command_spec.rb
+++ spec/lib/memcache/process_command_spec.rb
@@ -10,14 +10,13 @@ RSpec.describe Memcache::ProcessCommand do
   let(:get_command) { "get" }
   let(:key) { "test" }
   let(:flags) { "0" }
-  let(:exptime) { "1000" }
+  let(:exptime) { "-1" }
   let(:byte_count) { "4" }
   let(:should_reply) { "" }
   let(:data_block) { "test" }
-  let(:set_data) { "#{set_command} #{key} #{flags} #{exptime} #{byte_count} #{should_reply}\n#{data_block}\n" }
-  let(:get_data) { "#{get_command} #{key}\n" }
-  let(:request_data) { set_data }
-  let(:request) { Memcache::Request.new(request_data) }
+  let(:set_request) { Memcache::Request.new("#{set_command} #{key} #{flags} #{exptime} #{byte_count} #{should_reply}\n#{data_block}\n") }
+  let(:get_request) { Memcache::Request.new("#{get_command} #{key}\n") }
+  let(:request) {}
   let(:response) { Memcache::ProcessCommand.process_request(request) }
 
 =begin
@@ -54,14 +53,24 @@ END
 =end
 
   context 'when the command is SET' do
+    let(:request) { set_request }
     it 'succeeds' do
       expect(response).to eq("STORED\n")
     end
   end
   context 'when the command is GET' do
-    let(:request_data) { get_data }
+    let(:request) { get_request }
     it 'returns empty when no data' do
       expect(response).to eq("END\n")
+    end
+    context 'when there is data on the server' do
+      let(:exptime) { 100 }
+      before do
+        Memcache::ProcessCommand.process_request(set_request)
+      end
+      it 'returns the data' do
+        expect(response).to eq("VALUE test 0 4\ntest\nEND\n")
+      end
     end
   end
 end
